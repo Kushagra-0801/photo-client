0d2e5870d1c2349c5634cee4aaf9443cf5780a60
Kushagra Gupta <kushgpt08@gmail.com>
2024-11-27 04:09:36 +0530

    basic auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,52 @@
+import { signIn, signUp } from '@/lib/auth-client';
+
+import Form from 'next/form';
+
 export default function Home() {
+  const loginHandler = async (formData: FormData) => {
+    'use server';
+    // signIn()
+  };
+
+  const signUpHandler = async (formData: FormData) => {
+    'use server';
+
+    const email = formData.get('email')?.toString() ?? '';
+    const password = formData.get('password')?.toString() ?? '';
+    console.log(email, ':::', password);
+    const { data, error } = await signUp.email({
+      email, password, name: 'Kush',
+    }, {
+      onSuccess: (ctx) => {
+        alert('signed up')
+      }
+    })
+  };
+
   return (
-    <></>
+    <div>
+      <Form action={loginHandler}>
+        <label>
+          Email:
+          <input id='email' name='email' />
+        </label>
+        <label>
+          Password:
+          <input id='password' name='password' type='password' />
+        </label>
+        <button type='submit'>Log In</button>
+      </Form>
+      <Form action={signUpHandler}>
+        <label>
+          Email:
+          <input id='email' name='email' />
+        </label>
+        <label>
+          Password:
+          <input id='password' name='password' type='password' />
+        </label>
+        <button type='submit'>Sign Up</button>
+      </Form>
+    </div>
   );
 }
